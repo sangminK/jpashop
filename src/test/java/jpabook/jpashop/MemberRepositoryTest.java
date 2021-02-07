d1c6e470b01fb0fed2cf3d864b6c83f19f9f743e
sangminK <smkim2339@gmail.com>
2021-02-07 23:20:36 +0900

    h2 DB setting

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,4 +1,37 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MemberRepositoryTest {
-  
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional  // 테스트 끝나면 DB rollback
+    @Rollback(false) // rollback 안하고 commit 하려면 추
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);    // 같은 영속성 안에서는 id 값이 같으면 같은 엔티티로 식별
+    }
 }
