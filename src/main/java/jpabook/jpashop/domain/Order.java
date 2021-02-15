9679b52bc203ca4883fc24b5b08e8a7be2529306
sangminK <smkim2339@gmail.com>
2021-02-15 23:23:41 +0900

    주문, 카테고리 연관관계 메소드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,5 +33,19 @@ public class Order {
     @Enumerated(EnumType.STRING)  // ORDINAL(default) : 숫자, STRING : 문자
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
 
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
