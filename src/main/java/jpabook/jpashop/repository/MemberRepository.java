c58d5c850727b035811e2d615d5405b355485b08
sangminK <smkim2339@gmail.com>
2021-02-16 23:22:07 +0900

    회원 repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member); // 영속성 컨텍스트에 member 엔티티를 넣고, 트랜잭션이 커밋되는 시점에 DB에 반영(insert)
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // (type, PK)
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
