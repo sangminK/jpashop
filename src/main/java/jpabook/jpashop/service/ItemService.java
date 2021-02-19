eb8320d7ddced09f77289184ee7a083cb913a86b
sangminK <smkim2339@gmail.com>
2021-02-19 22:37:03 +0900

    상품 repository, service

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/*
+* 서비스가 단순하게 리포지토리로 위임만 한다면,
+* 컨트롤러에서 바로 리포지토리를 호출하는 것도 생각해 볼 필요가 있다!
+* */
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
