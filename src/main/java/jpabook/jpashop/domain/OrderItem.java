580bb662b1a74ec58dff5df2d24ee201d6891cd5
sangminK <smkim2339@gmail.com>
2021-02-20 22:54:54 +0900

    주문,주문상품 엔티티 : 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,27 @@ public class OrderItem {
 
     private int count;      // 주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // 주문 상품 생성과 동시에 재고를 까는 방식(무통장 입금이라면?)
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /*
+    * 주문상품 전체 가격 조회
+    * */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
