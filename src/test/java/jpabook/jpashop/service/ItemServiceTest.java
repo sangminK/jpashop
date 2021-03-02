08aca888d698a95d6e5a4bd9c88835f34ae5b7b1
sangminK <smkim2339@gmail.com>
2021-03-03 00:28:36 +0900

    상품 테스트

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -1,4 +1,36 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.item.Movie;
+import jpabook.jpashop.repository.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class ItemServiceTest {
-  
+
+    @Autowired ItemService itemService;
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void 상품등록() throws Exception {
+        // given
+        Item movie = new Movie();
+        movie.setName("다크나이트");
+
+        // when
+        Long saveId = itemService.saveItem(movie);
+
+        // then
+        assertEquals(movie, itemRepository.findOne(saveId));
+    }
+
 }
